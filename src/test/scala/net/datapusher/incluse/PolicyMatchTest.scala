a0546d01a621245692537be478541a5223363604
Haakon Nilsen <haakonn@gmail.com>
2012-02-26 20:54:19 +0100

    Add a test suite for path matching

---
+++ src/test/scala/net/datapusher/incluse/PolicyMatchTest.scala
@@ -0,0 +1,63 @@
+package net.datapusher.incluse
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+
+@RunWith(classOf[JUnitRunner])
+class PolicyMatchTest extends FunSuite {
+
+  def path(p: String) = p split("/") toList
+
+  val pathAbcd = path("a/b/c/d")
+
+  /**
+   * Human-readable version of this policy (% is wildcard in this case):
+   * <pre>
+   * +/%%
+   * -/%%/foo.txt
+   * -/tmp/backup/%
+   * +/tmp/backup/important
+   * -/var/cache
+   * </pre>
+   */
+  val samplePolicy = new Policy(List(
+      Named("var",List(Named("cache",List(),Some(false))),None),
+      RecWild(List(Named("foo.txt",List(),Some(false))),Some(true)),
+      Named("tmp",List(Named("backup",List(Named("important",List(),Some(true)), Wild(List(),Some(false))),None)),None)))
+  
+  test("All-inclusive policy matches") {
+    val allInclusive = new Policy(Seq(RecWild(accept=Some(true))))
+    assert(allInclusive matches pathAbcd)
+  }
+
+  test("All-exclusive policy does not match") {
+    val allExclusive = new Policy(Seq(RecWild(accept=Some(false))))
+    assert(!(allExclusive matches pathAbcd))
+  }
+
+  test("Explicitly included despite neighbouring non-recursive exclusive wildcard") {
+    assert(samplePolicy matches path("tmp/backup/important"))
+  }
+
+  test("Explicitly excluded") {
+    assert(!(samplePolicy matches path("var/cache")))
+  }
+
+  test("Implicitly included with straight match by recursive wildcard") {
+    assert(samplePolicy matches pathAbcd)
+  }
+
+  test("Implicitly included with backtracking involved") {
+    assert(samplePolicy matches path("tmp/backup/other/stuff"))
+  }
+
+  test("Implicitly excluded with straight match by non-recursive wildcard") {
+    assert(!(samplePolicy matches path("tmp/backup/nono")))
+  }
+
+  test("Explicitly excluded with straight match following recursive inclusive wildcard") {
+    assert(!(samplePolicy matches path("many/nodes/will/match/until/foo.txt")))
+  }
+  
+}
