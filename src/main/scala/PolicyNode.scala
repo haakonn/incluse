9f0cae5c0a341941cfa1073eb8adde36633558fc
Haakon Nilsen <haakonn@gmail.com>
2012-03-03 20:24:37 +0100

    Equals and hashCode for node types

--- src/main/scala/PolicyNode.scala
+++ src/main/scala/PolicyNode.scala
@@ -4,6 +4,11 @@ abstract sealed class PolicyNode(
     val children: Seq[PolicyNode],
     val accept: Option[Boolean]) {
   def cp(children: Seq[PolicyNode], accept: Option[Boolean]): PolicyNode
+  override def equals(that: Any) = that match {
+    case node: PolicyNode => accept == node.accept && children.toSet == node.children.toSet
+    case _ => false
+  }
+  override def hashCode = accept.hashCode() + children.hashCode()
 }
 
 case class Wild(
@@ -26,4 +31,9 @@ case class Named(
     override val accept: Option[Boolean] = None)
     extends PolicyNode(children, accept) {
   override def cp(c: Seq[PolicyNode], accept: Option[Boolean]) = copy(name, c, accept)
+  override def equals(that: Any) = that match {
+    case node: Named => name == node.name && super.equals(that)
+    case _ => false
+  }
+  override def hashCode() = name.hashCode() + super.hashCode()
 }
